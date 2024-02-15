4b2b1dda73d139bf8d3c83b05d97a36f7f85deee
J-Abdullah <83709737+J-Abdullah@users.noreply.github.com>
2024-02-15 16:43:34 +0500

    adjusting model functions

--- model_functions.py
+++ model_functions.py
@@ -2,7 +2,20 @@ import numpy as np
 import librosa #for audio processing
 import pandas as pd
 import pickle
+from tensorflow.keras.models import load_model
+from keras_self_attention import SeqSelfAttention
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Conv1D, MaxPooling1D, Input, BatchNormalization
 
+
+#loading models and encoders
+label_encoder=pickle.load(open(r'Serialized_objects\disease_encoder.pkl','rb'))
+
+d_model=load_model(r'Serialized_objects\RDM_Diagnose.h5',custom_objects={'SeqSelfAttention': SeqSelfAttention})
+
+user_encoder=pickle.load(open(r'Serialized_objects\user_data_encoder.pkl','rb'))
+
+prescription_model=pickle.load(open(r'Serialized_objects\medication_prescription.pkl','rb'))
 
 
 def feature_extraction(file,max_pad_len=926,n_features=52):
@@ -22,7 +35,24 @@ def feature_extraction(file,max_pad_len=926,n_features=52):
       
     return mfccs
 
+def diagnoser(file):
+    mfcc=feature_extraction(file)
+    disease=d_model(mfcc)
+    disease=label_encoder.inverse_transform(np.argmax(disease,axis=1))[0]
+    return disease
 
 
-def prep():
-    print("Preprocessing the data")
+
+def medicine_presciber(user_data):
+    #convert the user data to a dataframe
+    user_df=pd.DataFrame([user_data])
+
+    #encode the user data
+    categorical_data=user_df[['Gender','Smoking Status','Disease']]
+    numeric_data=user_df[['Age']]
+
+    categorical_data=user_encoder.transform(categorical_data).toarray()
+    features = np.hstack([numeric_data, categorical_data])
+    
+    medicine=prescription_model.predict(features)
+    return medicine
